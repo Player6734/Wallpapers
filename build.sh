e23a54c9e9a831e997bc75e99b8237c4102d7d3e
Player6734 <Player6734@gmail.com>
2023-11-18 20:41:38 +0100

    Sat Nov 18 20:41:36 CET 2023

--- build.sh
+++ build.sh
@@ -60,34 +60,22 @@ process_directory() {
 
 
 
-write_img_preview() {
-    local file_path="$1"
+process_directory() {
+    local dir_path="$1"
     local output_file="$2"
-    local preview_path="$PREVIEWS_DIR/$(basename "$file_path")"
-    local desired_height=200  # Set your desired height here for previews
+    local subdir_name=$(basename "$dir_path")
 
-    # Ensure the previews directory exists
-    mkdir -p "$PREVIEWS_DIR"
+    # Create a checkbox and a label for the directory
+    echo "<input type='checkbox' id='$subdir_name' style='display:none;'>" >> "$output_file"
+    echo "<label for='$subdir_name' style='font-size: 18px; color: #EBDBB2; cursor: pointer;'>$subdir_name</label>" >> "$output_file"
+    echo "<div style='display:none;' id='content_$subdir_name'>" >> "$output_file"
 
-    # Create a preview image if it doesn't exist
-    if [ ! -f "$preview_path" ]; then
-        local original_width=$(identify -format "%w" "$file_path")
-        local original_height=$(identify -format "%h" "$file_path")
+    # Process files in the current directory
+    while IFS= read -r -d '' file_info; do
+        write_img "$file_info" "$output_file"
+    done < <(find "$dir_path" -maxdepth 1 -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.webp" -o -name "*.avif" \) -print0)
 
-        if [ "$original_height" -ne 0 ]; then
-            local new_width=$((desired_height * original_width / original_height))
-            convert -strip "$file_path"  -quality 75 -resize "${new_width}x${desired_height}" "$preview_path"
-        else
-            echo "Error: Unable to read image dimensions for $file_path"
-            return  # Skip if unable to process the image
-        fi
-    fi
-
-    # Use the relative path for the preview in the HTML
-    local relative_preview_path=$(realpath --relative-to="$PICTURES_DIR" "$preview_path")
-
-    # Write the HTML for the preview, linking to the full-size image
-    echo "<a href=\"$file_path\"><img src=\"$relative_preview_path\" alt=\"$(basename "$file_path")\" height=\"$desired_height\"></a>" >> "$output_file"
+    echo "</div>" >> "$output_file"
 }
 
 
