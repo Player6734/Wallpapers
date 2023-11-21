ca4f17e77870f822bb1b449a60bd8f859031e4fc
Player6734 <Player6734@gmail.com>
2023-11-21 12:49:07 +0100

    Tue Nov 21 12:49:06 CET 2023

--- pictures-build.sh
+++ pictures-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PICTURES_DIR=$(pwd)
-PREVIEWS_DIR="$PICTURES_DIR/.preview"
+PREVIEWS_DIR="$PICTURES_DIR/.previews"
 SUBDIR_HTML_DIR="subdir-html"  # Name of the new directory to store HTML files
 mkdir -p "$SUBDIR_HTML_DIR"
 mkdir -p "$PREVIEWS_DIR"
